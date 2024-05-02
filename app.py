0e067bfd18d6a052b320e8f794dd81ef2e835b5f
jhacksman <167836839+jhacksman@users.noreply.github.com>
2024-05-02 13:40:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-import os
-import streamlit as st
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import GPT4AllEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import JsonOutputParser
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import JsonOutputParser
 from langchain import hub
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
 from typing_extensions import TypedDict
 from typing import List
 from langchain.schema import Document
 from langgraph.graph import END, StateGraph
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.schema import Document
 from langchain_community.document_loaders import PyPDFLoader
+import streamlit as st
+import os
 
 # Section 1: Setup the local LLM and API key for Tavily search engine, set title and input for Streamlit
 local_llm = "dolphin-llama3:8b"
